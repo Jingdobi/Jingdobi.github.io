1d18fbf0862187235a9bb62d933592935b1fdfa0
Jingdobi <porterthemerrell2gmail.com>
2020-01-14 11:31:50 -0700

    initial commit

--- Scripts/currentDate.js
+++ Scripts/currentDate.js
@@ -5,4 +5,3 @@ let currentYear = currentDate.getFullYear();
 console.log(currentYear);
 
 document.getElementById("currentYear").innerHTML = currentYear;
-document.getElementById("currentDate").innerHTML = currentDate;
